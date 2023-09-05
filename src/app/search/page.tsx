ae0497d2246a7d943919f4451112ee10f6dc67e6
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-09-05 23:22:43 +0900

    display content previously

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,6 +3,7 @@ import ArticleCard from "@/components/articlecard";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import SearchField from "@/components/search";
 import { allPosts } from "contentlayer/generated";
+import { all } from "axios";
 
 interface SearchProps {
   searchParams: {
@@ -29,23 +30,37 @@ function getPostsFromParams(searchParams: SearchProps["searchParams"]) {
     });
 }
 
-const SearchPage: React.FC<SearchProps> = ({ searchParams }) => {
-  const articles = getPostsFromParams(searchParams);
+const SearchPage: React.FC<SearchProps> = async ({ searchParams }) => {
+  const articles = searchParams.q?.length !== 0 ? await getPostsFromParams(searchParams) : await allPosts.map((post) => {
+    const article = {
+      id: post._id,
+      title: post.title,
+      slug: post.slug,
+      icon: post.icon,
+      description: post.description,
+      date: post.date,
+      categories: post.categories,
+    };
+    return article;
+  });
   return (
     <main className="flex flex-col bg-white dark:bg-gray-900 h-screen py-24 justify-cente items-center px-8 sm:px-20 lg:px-40 mx-auto">
       <SearchField />
       <div className="grid gap-y-8 sm:gap-12 grid-cols-1 sm:grid-cols-2"></div>
-      <div className="flex flex-row items-center">
-        <SentimentVeryDissatisfiedIcon className="text-2xl mr-2" />
-        <h1 className="text-md sm:text-2xl my-4 text-center w-full">
-          {"Not Found, Please try Again another keyword."}
-        </h1>
-      </div>
-      <div className="grid gap-y-8 sm:gap-16 grid-cols-1 sm:grid-cols-2">
-        {articles.map((article) => (
-          <ArticleCard article={article} />
-        ))}
-      </div>
+      {articles.length === 0 ? (
+        <div className="flex flex-row items-center">
+          <SentimentVeryDissatisfiedIcon className="text-2xl mr-2" />
+          <h1 className="text-md sm:text-2xl my-4 text-center w-full">
+            {"Not Found, Please try Again another keyword."}
+          </h1>
+        </div>
+      ) : (
+        <div className="grid gap-y-8 sm:gap-16 grid-cols-1 sm:grid-cols-2 mt-8">
+          {articles.map((article) => (
+            <ArticleCard article={article} />
+          ))}
+        </div>
+      )}
     </main>
   );
 };
