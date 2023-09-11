e39ca9cb16198dbfd8f8ec9d469366063eace453
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-09-11 14:50:35 +0900

    add aritcle

---
+++ src/stories/articlecard.stories.ts
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from "@storybook/react";
+import ArticleCard from "../components/articlecard";
+
+const meta = {
+  title: "Example/ArticleCard",
+  component: ArticleCard,
+  argTypes: {},
+} as Meta<typeof ArticleCard>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {},
+};
---
+++ src/stories/themeSwitch.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ThemeSwitch from "../components/themeSwitch";
+
+const meta = {
+    title: "Example/ThemeSwitch",
+    component: ThemeSwitch,
+    argTypes: {
+    },
+} as Meta<typeof ThemeSwitch>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        Primary: true,
+    },
+};
