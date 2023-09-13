6cdabe7f3ce41e42858960045119209da7f16daa
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-09-13 17:09:27 +0900

    improve searchPaformance

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect, Suspense, use } from "react";
 import ArticleCard from "@/components/articlecard";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import SearchField from "@/components/search";
@@ -15,37 +15,36 @@ interface SearchProps {
 
 const SearchPage: React.FC<SearchProps> = ({ searchParams }) => {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [searchResult, setSearchResult] = useState<Article[]>([]);
   useEffect(() => {
-    async function getPostsFromParams(
-      searchParams: SearchProps["searchParams"]
-    ) {
-      const articles = await getAllPosts();
-      return articles.filter((article) => {
+    (async () => {
+      const res = await getAllPosts();
+      setArticles(res);
+    })();
+  }, []);
+  useEffect(() => {
+    (async () => {
+      const result = articles.filter((article) => {
         return article.title
           .toLowerCase()
           .includes(searchParams?.q?.toLowerCase());
       });
-    }
-    (async () => {
-      const articles = searchParams.q
-        ? await getPostsFromParams(searchParams)
-        : await getAllPosts();
-      setArticles(articles);
+      setSearchResult(result);
     })();
-  }, [searchParams]);
+  }, [searchParams, articles]);
   return (
     <Suspense fallback={<Loading />}>
       <main className="flex flex-col bg-white dark:bg-gray-900 min-h-screen py-24 justify-cente items-center px-8 sm:px-20 lg:px-40 mx-auto">
         <SearchField />
         <div className="grid gap-y-8 sm:gap-12 grid-cols-1 sm:grid-cols-2"></div>
-        {articles.length === 0 && searchParams.q ? (
+        {searchResult.length === 0 ? (
           <div className="flex flex-row items-center">
             <SentimentVeryDissatisfiedIcon className="text-2xl mr-2" />
             <h1 className="text-md sm:text-2xl my-4 text-center w-full">
               {"Not Found, Please try Again another keyword."}
             </h1>
           </div>
-            ) : (
+        ) : (
           <div className="grid gap-y-8 sm:gap-16 grid-cols-1 sm:grid-cols-2 mt-8">
             {articles.map((article, index) => (
               <React.Fragment key={index}>
