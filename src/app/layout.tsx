f51f8f9d6c9533cd7ba8a49d41ab923ef4270cb5
sasakitimaru <84312254+sasakitimaru@users.noreply.github.com>
2023-09-16 18:17:08 +0900

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -78,6 +79,7 @@ export default function RootLayout({
             )}
           >
             {children}
+            <Analytics />
           </main>
           <Footer />
         </Providers>
